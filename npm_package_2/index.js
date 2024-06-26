ea1f3dd9df061a55dcd0bdf2e0f3b60022f2b165
Pratik Dayma <pratikdayma45@gamil.com>
2024-06-26 22:27:59 +0530

    updated operation on array package file index.js

--- npm_package_2/index.js
+++ npm_package_2/index.js
@@ -1,7 +1,39 @@
-module.exports=function getSum(arr) {
-    let sum=0;
-    for(let i=0;i<arr.length;i++){
-        sum += arr[i];
-    }
-    console.log(sum);
-}
+module.exports = {
+    findMaxItem,
+    findMinItem,
+    sumOfArray,
+    averageOfArray,
+    reverseAnArray,
+    findUniqueElements,
+    mergeTwoArrays,
+  };
+
+
+
+  function findMaxItem(arr) {
+    return Math.max(...arr);
+  }
+  
+  function findMinItem(arr) {
+    return Math.min(...arr);
+  }
+  
+  function sumOfArray(arr) {
+    return arr.reduce((acc, val) => acc + val, 0);
+  }
+  
+  function averageOfArray(arr) {
+    return arr.length ? sumOfArray(arr) / arr.length : 0;
+  }
+  
+  function reverseAnArray(arr) {
+    return arr.slice().reverse();
+  }
+  
+  function findUniqueElements(arr) {
+    return [...new Set(arr)];
+  }
+  
+  function mergeTwoArrays(arr1, arr2) {
+    return [...arr1, ...arr2];
+  }
